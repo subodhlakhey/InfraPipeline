8bb2bea5925fa7703d9853b383ab4a1271c1083a
subodhlakhey <37637668+subodhlakhey@users.noreply.github.com>
2021-08-16 19:16:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
     storage_account_name = "xortfstate01"
     container_name       = "defaultrg"
     key                  = "dev.tfstate"
-    access_key           = "LPs2BPXpBxTekaOU/yQwGEMIlBjJA4vYIxekA2iJFLdTmmaHCxWVzKgsgb3uEeTXh/tmTiu9KQueZmzygvpx3Q=="
+    access_key           = var.tf_state_access_key
   }
 }
 
@@ -15,7 +15,7 @@ provider "azurerm" {
   # skip provider rego because we are using a service principal with limited access to Azure
   subscription_id            = "473c3c84-a93a-48ff-b941-a43159f5f9b7"
   client_id                  = "94636534-be4e-4253-9e7b-beb0ba5acda8"
-  client_secret              = "_-8Yea~6.7jspd89.FDqN_9b-to063aYp_"
+  client_secret              = var.client_secret
   tenant_id                  = "7c10e4ef-1eb3-403a-9af7-c0aae38648ef"
   skip_provider_registration = "true"
   features {}
